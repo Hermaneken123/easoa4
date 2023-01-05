f622a0b983c803b49f663c3e508ba9906a1f0cc3
Hermaneken123 <hermanekett@gmail.com>
2023-01-05 23:39:57 +0100

    allt i .js är typ klart yey

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -8,23 +8,24 @@ function fetchData() {
 
         if (!timeEditUrl.endsWith('.json')) timeEditUrl = timeEditUrl.concat('.json');  
 
-       // window.alert(timeEditUrl); 
+       // window.alert(timeEditUrl);
+       // https://cloud.timeedit.net/ltu/web/schedule1/ri105656X45Z0XQ6Z36g1Y40y3036Y32107gQY6Q547520876YQ83.html
 
         $.getJSON(timeEditUrl, function (data) {
             for (let i = 0; i < data.reservations.length; i++) {
                 //console.log(data.reservations[i]);
                 var text = `<tr>                    
-                <td>${data.reservations[i].id}</td>
+                <td class="id">${data.reservations[i].id}</td>
                 <td>${data.reservations[i].columns[2]}</td>
                 <td>${data.reservations[i].startdate}</td>
                 <td>${data.reservations[i].starttime}</td>
                 <td>${data.reservations[i].endtime}</td>
                 <td>${data.reservations[i].columns[7]}</td>
-                <td class="tcell"><input type="text" disabled></td>
+                <td class="info"><input type="text" disabled></td>
                 </tr>`;
 
                 $("#currentLessons").append(text);
-                
+               
                 lesson.push({
                     "id": data.reservations[i].id,
                     "location": data.reservations[i].columns[2],
@@ -32,57 +33,42 @@ function fetchData() {
                     "starttime": data.reservations[i].starttime,
                     "endtime": data.reservations[i].endtime,
                     "description": data.reservations[i].columns[7],
-                    "info": "" 
+                    "info": ""
                 });
-            }    
-            // for(let i=0; i< lesson.length; i++) {
-            //     console.log(lesson[i]);
-            //     lesson[i].info = "irgor";
-           // }   
+            }
         });
-      
+     
     } else {
     console.log("stop");
-    }     
+    }
 }
 
 function enableEdit() {
-    var inputs = document.getElementsByClassName('tcell');
+    var inputs = document.getElementsByClassName('info');
     for(var i = 0; i < inputs.length; i++) {
-    inputs[i].firstChild.disabled = false;
-   // console.log(inputs[i].firstChild);
+        inputs[i].firstChild.disabled = false;
     }
 }
 
 function saveLessons() {
     var table = document.getElementById('currentLessons');
-    var inputs = document.getElementsByClassName('tcell');
-    // for(var i = 0; i < table.rows[i]; i++) {
-    //     //console.log(table.rows[i]);
-    //     if(lesson.info !== "" && currentLessons.id == lesson[id] ) {
-    //         lesson[i].info = "gdgdg";
-    //     }
-        for(let i=0; i< lesson.length; i++) {
-            while(lesson.info == !null && currentLessons.id == lesson.id ) {
-            lesson.info = '';
-        }     
-        console.log(lesson.info);
-    }
-    for(var i = 0; i < inputs.length; i++) {
-        inputs[i].firstChild.disabled = true;
-    }
+    // debugger;
     for(let row of table.rows) {
-       //console.log(row);
-       
-        
-    }
-}
+        // console.log(row);
+        const id = row.querySelector('.id').innerHTML;
+        let infoElement = row.querySelector('.info input');
+        infoElement.disabled = true;
+        let info = infoElement.value;
 
-// för varje rad i tabellen där man lägger till ett värde "info" måste man spara id:t från den raden för att kunna matcha mot arrayen(lesson)
-// if info != empty check if id=id []lesson
-//when match update = lesson[i].info = "";
-// därefter måste man loopa igenom lesson-arrayen, matcha id"828528) if id==id []; från tabellen med id från arrayen
-// och när de matchar så uppdaterar du info-proertyn med lesson[i].info = "foo" 
+        for (let i = 0; i < lesson.length; i++) {
+            if (id == lesson[i].id) {
+                lesson[i].info = info;
+            }
+        }
+    }
+
+    console.log(lesson);
+}
 
 
 async function pushLesson() {
@@ -93,11 +79,8 @@ async function pushLesson() {
         "Starttime": data.reservations[i].starttime,
         "Endtime": data.reservations[i].endtime,
         "Description": data.reservations[i].columns[7],
-        "Info": "" 
+        "Info": ""
     });
 
     window.alert(lessonList);
 }
-
-//storeLesson();
-//how to create js object
